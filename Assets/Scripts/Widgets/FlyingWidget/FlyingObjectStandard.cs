bb706252cc06808ca6012ad16076e24b914a55f1
zjq <1477194584@qq.com>
2021-08-07 20:47:47 +0800

    add bullet prefab and bullet flying logic

--- Assets/Scripts/Widgets/FlyingWidget/FlyingObjectStandard.cs
+++ Assets/Scripts/Widgets/FlyingWidget/FlyingObjectStandard.cs
@@ -6,7 +6,56 @@ namespace Unity.NJUCS.Widget
 {
     public class FlyingObjectStandard : FlyingObjectBase
     {
-        
+        [Header("General")][Tooltip("子弹生存时间")]
+        public float MaxLifeTime = 5f;
+
+
+        [Header("Movement")]
+        [Tooltip("子弹速度")]
+        public float Speed = 20f;
+
+        [Tooltip("子弹受到的重力加速度")]
+        public float GravityDownAcceleration = 0f;
+
+        [Tooltip("是否计算武器本身的速度")]
+        public bool InheritWeaponVelocity = false;
+
+        FlyingObjectBase m_FlyingObjectBase;
+        Vector3 m_Velocity;
+        float m_ShootTime;
+
+        void OnEnable()
+        {
+            m_FlyingObjectBase = GetComponent<FlyingObjectBase>();
+            m_FlyingObjectBase.OnShoot += OnShoot;
+            Destroy(gameObject, MaxLifeTime);
+        }
+
+        new void OnShoot()
+        {
+            m_ShootTime = Time.time;
+            m_Velocity = transform.forward * Speed;
+            transform.position += m_FlyingObjectBase.InheritedMuzzleVelocity * Time.deltaTime;
+        }
+
+        void Update()
+        {
+            // Move
+            transform.position += m_Velocity * Time.deltaTime;
+            if (InheritWeaponVelocity)
+            {
+                transform.position += m_FlyingObjectBase.InheritedMuzzleVelocity * Time.deltaTime;
+            }
+
+            // Gravity
+            if (GravityDownAcceleration > 0)
+            {
+                // add gravity to the projectile velocity for ballistic effect
+                m_Velocity += Vector3.down * GravityDownAcceleration * Time.deltaTime;
+            }
+
+        }
+
     }
 }
 
